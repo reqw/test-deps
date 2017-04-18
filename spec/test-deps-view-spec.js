83ee7bbf4750a7e8d9791f11dbfe0f44bb12810c
reqw <unk>
2017-04-18 17:38:56 +0300

    Initial

---
+++ spec/test-deps-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TestDepsView from '../lib/test-deps-view';
+
+describe('TestDepsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
